5412cc9835187ce5cd7b6994edee40504bbc859c
Lvnge <roy9096@hotmail.com>
2026-03-27 17:20:07 -0600

    fix backend deployment

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,9 +3,10 @@ import axios from "axios";
 const api = axios.create({ baseURL: "http://localhost:3000" });
 
 export const getQuotes = () => api.get("/quotes");
-export const createQuote = (data) => api.post("/quotes", data);
-export const updateQuote = (id, data) => api.put(`/quotes/${id}`, data);
+export const createQuote = (data: object) => api.post("/quotes", data);
+export const updateQuote = (id: number, data: object) =>
+  api.put(`/quotes/${id}`, data);
 export const getClients = () => api.get("/clients");
-export const createClient = (data) => api.post("/clients", data);
+export const createClient = (data: object) => api.post("/clients", data);
 export const getCatalog = () => api.get("/catalog");
-export const createCatalogItem = (data) => api.post("/catalog", data);
+export const createCatalogItem = (data: object) => api.post("/catalog", data);
